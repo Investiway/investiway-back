a6159e6accf4ab26fba7be5b5ff64502c062a3d1
wawahuy <kakahuy99@gmail.com>
2023-04-02 23:16:26 +0700

    [update] mail

---
+++ apps/investiway/src/mail/mail.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import {MailerModule} from "@nestjs-modules/mailer";
+import { MailService } from './mail.service';
+import {MailConfig} from "../configs/mail.config";
+
+@Module({
+  imports: [
+    MailerModule.forRootAsync({
+      useClass: MailConfig
+    })
+  ],
+  providers: [MailService]
+})
+export class MailModule {}
